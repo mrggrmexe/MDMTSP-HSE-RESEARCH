6515ca82f777a920c5019a52dd93278f38db0ce6
Professor <100163658+mrggrmexe@users.noreply.github.com>
2026-05-08 00:37:39 +0300

    fix( TL bug )

--- src/mdmtsp/nearest_neighbour_2opt.cpp
+++ src/mdmtsp/nearest_neighbour_2opt.cpp
@@ -1,112 +1,71 @@
 #include "mdmtsp_solver.hpp"
 
 #include <algorithm>
-#include <vector>
+#include <cstddef>
 
 namespace mdmtsp {
 
 namespace {
 
-[[nodiscard]] bool improve_two_opt_closed(Route& route, const DistanceMatrix& matrix) {
+constexpr cost_t kEps = static_cast<cost_t>(1e-9);
+constexpr int kMaxPassesPerRoute = 4;
+constexpr std::size_t kMaxChecksPerRoute = 2'000'000;
+
+[[nodiscard]] bool try_two_opt_pass(Route& route,
+                                    const DistanceMatrix& matrix,
+                                    const bool return_to_depot,
+                                    std::size_t& checks) {
     if (route.size() < 4) {
         return false;
     }
 
-    bool improved_any = false;
+    const std::size_t last = return_to_depot ? route.size() - 1 : route.size();
 
-    for (;;) {
-        cost_t best_delta = static_cast<cost_t>(0);
-        std::size_t best_i = 0;
-        std::size_t best_k = 0;
+    for (std::size_t i = 1; i + 1 < last; ++i) {
+        for (std::size_t k = i + 1; k < last; ++k) {
+            if (++checks > kMaxChecksPerRoute) {
+                return false;
+            }
 
-        for (std::size_t i = 1; i + 2 < route.size(); ++i) {
-            for (std::size_t k = i + 1; k + 1 < route.size(); ++k) {
-                const node_id_t a = route[i - 1];
-                const node_id_t b = route[i];
-                const node_id_t c = route[k];
+            const node_id_t a = route[i - 1];
+            const node_id_t b = route[i];
+            const node_id_t c = route[k];
+
+            cost_t delta = static_cast<cost_t>(0);
+
+            if (k + 1 < route.size()) {
                 const node_id_t d = route[k + 1];
-
-                const cost_t delta =
-                    matrix[a][c] + matrix[b][d] - matrix[a][b] - matrix[c][d];
-
-                if (delta < best_delta) {
-                    best_delta = delta;
-                    best_i = i;
-                    best_k = k;
+                delta = matrix[a][c] + matrix[b][d] - matrix[a][b] - matrix[c][d];
+            } else {
+                if (return_to_depot) {
+                    continue;
                 }
+                delta = matrix[a][c] - matrix[a][b];
+            }
+
+            if (delta < -kEps) {
+                std::reverse(route.begin() + static_cast<std::ptrdiff_t>(i),
+                             route.begin() + static_cast<std::ptrdiff_t>(k + 1));
+                return true;
             }
         }
-
-        if (!(best_delta < static_cast<cost_t>(0))) {
-            break;
-        }
-
-        std::reverse(route.begin() + static_cast<std::ptrdiff_t>(best_i),
-                     route.begin() + static_cast<std::ptrdiff_t>(best_k + 1));
-        improved_any = true;
     }
 
-    return improved_any;
+    return false;
 }
 
-[[nodiscard]] bool improve_two_opt_open(Route& route, const DistanceMatrix& matrix) {
-    if (route.size() < 3) {
-        return false;
-    }
+void improve_route_with_limited_two_opt(Route& route,
+                                        const DistanceMatrix& matrix,
+                                        const bool return_to_depot) {
+    std::size_t checks = 0;
 
-    bool improved_any = false;
-
-    for (;;) {
-        cost_t best_delta = static_cast<cost_t>(0);
-        std::size_t best_i = 0;
-        std::size_t best_k = 0;
-
-        for (std::size_t i = 1; i + 1 < route.size(); ++i) {
-            for (std::size_t k = i + 1; k < route.size(); ++k) {
-                const node_id_t a = route[i - 1];
-                const node_id_t b = route[i];
-                const node_id_t c = route[k];
-
-                cost_t delta = static_cast<cost_t>(0);
-
-                if (k + 1 < route.size()) {
-                    const node_id_t d = route[k + 1];
-                    delta = matrix[a][c] + matrix[b][d] - matrix[a][b] - matrix[c][d];
-                } else {
-                    delta = matrix[a][c] - matrix[a][b];
-                }
-
-                if (delta < best_delta) {
-                    best_delta = delta;
-                    best_i = i;
-                    best_k = k;
-                }
-            }
-        }
-
-        if (!(best_delta < static_cast<cost_t>(0))) {
+    for (int pass = 0; pass < kMaxPassesPerRoute; ++pass) {
+        if (!try_two_opt_pass(route, matrix, return_to_depot, checks)) {
             break;
         }
-
-        std::reverse(route.begin() + static_cast<std::ptrdiff_t>(best_i),
-                     route.begin() + static_cast<std::ptrdiff_t>(best_k + 1));
-        improved_any = true;
-    }
-
-    return improved_any;
-}
-
-void improve_route_with_two_opt(Route& route,
-                                const DistanceMatrix& matrix,
-                                const bool return_to_depot) {
-    if (route.empty()) {
-        return;
-    }
-
-    if (return_to_depot) {
-        improve_two_opt_closed(route, matrix);
-    } else {
-        improve_two_opt_open(route, matrix);
+        if (checks > kMaxChecksPerRoute) {
+            break;
+        }
     }
 }
 
@@ -122,7 +81,7 @@ void improve_route_with_two_opt(Route& route,
     const auto matrix = instance.build_distance_matrix();
 
     for (auto& route : solution.routes) {
-        improve_route_with_two_opt(route.nodes, matrix, instance.return_to_depot);
+        improve_route_with_limited_two_opt(route.nodes, matrix, instance.return_to_depot);
     }
 
     solution.objective = compute_objective(solution, instance, matrix);
